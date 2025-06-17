e866f05dd6e1b8bd66de40e2d81fbaae74454958
prasad-bigdp <bigdp585@gmail.com>
2025-06-17 09:00:20 +0530

    update

---
+++ JS/code/datatypes/script.js
@@ -0,0 +1,30 @@
+let a = 2
+console.log(a)
+console.log(typeof a); //number
+a = "prasad";
+console.log(typeof a); //string
+let b = 10.8145;
+console.log(b.toFixed(2)) // round figure the value upto 2 digits after decimal
+console.log(b.toPrecision(3)) // round figure the value upto total digits
+b = 25;
+console.log(b.toString(2))// converts number to string of binary
+
+//Math Methods
+console.log(Math.max(5, 6, 1, 2, 9)) // returns maximum value
+console.log(Math.min(5, 6, 1, 2, 9))  // returns minimum value
+console.log(Math.pow(2, 3))  // returns a power b
+console.log(Math.sqrt(16)) //returns square root
+console.log(Math.cbrt(27)) //returns cube root
+console.log(Math.abs(-10)) //returns always positive value
+console.log(Math.ceil(-1.16)) // returns nearest larger value
+console.log(Math.floor(1.8)) // returns nearest lower value
+console.log(Math.round(2.53)) //rounds upto nearest integer
+console.log(Math.round(Math.random() * 10000)) // returns random number between 0 to 1
+// generate 4 digit OTP using math random
+// generate dice value in ludo
+console.log(Math.ceil(Math.random() * 6))
+
+
+
+
+
