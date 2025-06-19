54f1bd8aef5b39d4d646bb4e2dc48bcc9c01f74c
prasad-bigdp <bigdp585@gmail.com>
2025-06-19 08:48:34 +0530

    update

---
+++ JS/code/decision-making/script.js
@@ -0,0 +1,40 @@
+let name = "prasad"
+if (name == "prasad")
+{
+    console.log("welcome prasad")
+}
+let age = 16;
+if (age >= 18)
+{
+    console.log("you are eligible to vote")
+}
+else
+{
+    console.log("you are not eligible to vote")
+}
+
+let num = 21;
+if (num > 0)
+{
+    console.log(`${num} is positive`)
+}
+else if (num < 0)
+{
+    console.log(`${num} is negitive`)
+}
+else
+{
+    console.log("The numer is zero")
+}
+let day = 3;
+switch (day)
+{
+    case 1: console.log("Monday"); break;
+    case 2: console.log("Tuesday"); break;
+    case 3: console.log("wednesday"); break;
+    case 4: console.log("Thursday"); break;
+    case 5: console.log("Friday"); break;
+    case 6: console.log("Saturday"); break;
+    case 7: console.log("Sunday"); break;
+    default: console.log("enter value between 1-6")
+}
