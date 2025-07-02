a798063b41e2f85d56a3838d769bd82e2dad34c3
prasad-bigdp <bigdp585@gmail.com>
2025-07-02 08:25:46 +0530

    update

---
+++ JS/code/DOM3/script.js
@@ -0,0 +1,56 @@
+// function createPara ()
+// {
+//     const para = document.createElement('p');
+//     para.textContent = "PRASAD"
+//     document.body.appendChild(para)
+// }
+// function createPara2 ()
+// {
+//     for (let i = 0; i <= 10; i++)
+//     {
+//     const para = document.createElement('p'); //<p></p>
+//     para.textContent = "PRASAD" //
+//     document.body.appendChild(para)
+//     }
+// }
+// createPara2();
+
+
+
+
+
+
+
+const movies = [
+    {
+       Title: "RRR"
+    },
+    {
+       Title:"Pushpa"
+    },
+    {
+       Title:"Salaar"
+    }
+]
+const mainEle = document.getElementById('main');
+function createMovies ()
+{
+    movies.forEach(function (ele,ind)
+    {
+        
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
